b4f24f3cb546845ac93fae3ccd8abe5b06923ebc
yalp <hsahin5801@gmail.com>
2024-02-14 22:49:58 +0300

    commit_2

--- turkdeniz/turkdeniz/settings.py
+++ turkdeniz/turkdeniz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q^3816_iav9r&qgbe1#+5$^4@1khz-g6q-7na46^cj!mb#g-eh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_URLS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_URLS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATIC_ROOT =  BASE_DIR / 'staticfiles'
 # MEDIA_ROOT = BASE_DIR / 'uploads'
